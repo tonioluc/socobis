cb59b7e13749b3a042e1e1a1a452f6cdb2d50a35
antonio <antoniorandria3342@gmail.com>
2026-01-18 13:36:30 +0300

    Add initial project structure with Maven wrapper and Spring Boot application

---
+++ socobis-back/src/main/java/itu/socobis/back/SocobisBackApplication.java
@@ -0,0 +1,13 @@
+package itu.socobis.back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocobisBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocobisBackApplication.class, args);
+	}
+
+}
---
+++ socobis-back/src/test/java/itu/socobis/back/SocobisBackApplicationTests.java
@@ -0,0 +1,13 @@
+package itu.socobis.back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocobisBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
